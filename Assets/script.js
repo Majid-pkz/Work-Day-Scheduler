f3daf43486f0e9b11069788c435337ebc20bcf56
Majid-pkz <majid.computing@gmail.com>
2022-12-21 10:50:56 +1100

    present past future classes have been added

--- Assets/script.js
+++ Assets/script.js
@@ -21,3 +21,65 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+var timeDisplayEl = $('#currentDay');
+
+
+//handle displaying the time
+function displayTime() {
+  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+displayTime();
+setInterval(displayTime, 1000);
+
+
+
+var currentTime=(dayjs().format('H'));
+//working     hours start from 9am-5pm(
+ var numberOfTimeBlock=$('#container').children('div').length;
+
+for(var i=0;i<numberOfTimeBlock;i++){
+  var timeBlockSpanContent=$('#container').children().eq(i).children('span')[0].innerHTML
+  if(timeBlockSpanContent ==currentTime){
+    for(var j=i+1;j<numberOfTimeBlock;j++){
+         
+                for(var k=i-1;k>=0;k--){
+                  $('#container').children().eq(i).addClass('present')
+                  var state=$('#container').children().eq(j).addClass('future');
+                  var state1=$('#container').children().eq(k).addClass('past')
+                }
+              }       
+    
+  }
+ if(currentTime>17&& currentTime<24){
+  for(var k=0;k<numberOfTimeBlock;k++){
+    $('#container').children().eq(k).addClass('past')
+      }
+  }
+ else{
+    for(var k=0;k<numberOfTimeBlock;k++){
+      $('#container').children().eq(k).addClass('future')
+        }
+ }
+}
+
+
+var saveBtnEl=$('#container').children('div').children('button');
+
+saveBtnEl.on('click', function (event){
+ console.log($(event.target))
+})
+
+
+var tasks=["",""]
+
+var task = $('textarea[class="description"]');
+
+
+
+
+
+
+
+
