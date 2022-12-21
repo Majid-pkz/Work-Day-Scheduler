352d1ebb33ab428803ddfd4207285310ba333179
Majid-pkz <majid.computing@gmail.com>
2022-12-21 13:57:51 +1100

    Final-save button is functional now and tasks  for each time block is displayed

--- Assets/script.js
+++ Assets/script.js
@@ -1,26 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+
 
 var timeDisplayEl = $('#currentDay');
 
@@ -57,29 +35,29 @@ for(var i=0;i<numberOfTimeBlock;i++){
     $('#container').children().eq(k).addClass('past')
       }
   }
- else{
+ if(currentTime<9){
     for(var k=0;k<numberOfTimeBlock;k++){
       $('#container').children().eq(k).addClass('future')
         }
  }
 }
 
+$(".saveBtn").on("click", function () {
+  
+  var taskInput = $(this).siblings(".description").val();
+  var timeSlot = $(this).parent().attr("id");
 
-var saveBtnEl=$('#container').children('div').children('button');
-
-saveBtnEl.on('click', function (event){
- console.log($(event.target))
+  // Save tasks in local storage
+  localStorage.setItem(timeSlot, taskInput);
 })
 
+// get the task description for each time block from localstorage
 
-var tasks=["",""]
-
-var task = $('textarea[class="description"]');
-
-
-
-
+$('textarea').each(function(){
+  var key=$(this).parent()[0].id;  
+ console.log(key )
+ var taskDescription= localStorage.getItem(key)
 
 
-
-
+  $(this).text(taskDescription);
+})
